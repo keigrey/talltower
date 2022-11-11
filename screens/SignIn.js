4fab89355ff72bf8620679b290e49d0aacb8dceb
keigrey <keixgrey@gmail.com>
2022-11-11 20:27:51 +0900

    feat: Add Context to SignIn

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,10 +1,24 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Context from "../context/Context";
+import { useContext } from "react";
 
 export default function SignIn() {
+  const {
+    theme: { colors },
+  } = useContext(Context);
   return (
-    <View>
-      <Text>SignIn Text</Text>
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: colors.background,
+      }}
+    >
+      <Text style={{ color: colors.text, fontSize: 24, marginBottom: 20 }}>
+        Welcome to Talltower
+      </Text>
     </View>
   );
 }
