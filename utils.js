2662a66079e7e6a38fcf429fe7177e7ab4a6a964
keigrey <keixgrey@gmail.com>
2022-11-11 20:25:26 +0900

    style: Add color palette

---
+++ utils.js
@@ -0,0 +1,26 @@
+const palette = {
+  jet: "#0b0c0b", //background
+  onyx: "#181818",
+  shadow: "#1f201f", // text box dark, search box
+  mist: "#333433", // text on dark
+  ice: "#cdd8dd", //text box light
+  snow: "#e7ebeb", // header text white
+  charcoal: "#080a09", // header text black
+  mint: "#58ad89", // select green
+  heather: "#8654ad", // select purple
+  black: "#000000",
+};
+
+export const theme = {
+  colors: {
+    background: palette.jet,
+    // foreground: palette.tealGreenDark,
+    primary: palette.jet,
+    tertiary: palette.mist,
+    secondary: palette.shadow,
+    white: palette.ice,
+    text: palette.snow,
+    secondaryText: palette.charcoal,
+    iconGray: palette.mint,
+  },
+};
