6ae3da52ecb0804d5ad3ee5624d77a9c6771517a
keigrey <keixgrey@gmail.com>
2022-11-12 20:22:53 +0900

    feat: Add photo select and name input touchables to Profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,8 +1,9 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image, TextInput } from "react-native";
 import React, { useContext, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import Constants from "expo-constants";
 import Context from "../context/Context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Profile() {
   const {
@@ -10,6 +11,7 @@ export default function Profile() {
   } = useContext(Context);
 
   const [displayName, setDisplayName] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <React.Fragment>
@@ -29,9 +31,41 @@ export default function Profile() {
         <Text style={{ fontSize: 14, color: colors.backgound, marginTop: 20 }}>
           Please provide your name and an optional profile photo
         </Text>
-        <TouchableOpacity>
-          <Text>Hi</Text>
+        <TouchableOpacity
+          style={{
+            marginTop: 30,
+            width: 120,
+            height: 120,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 120,
+            backgroundColor: "red",
+          }}
+        >
+          {!selectedImage ? (
+            <MaterialCommunityIcons
+              name="camera-plus"
+              color={colors.iconGray}
+              size={45}
+            />
+          ) : (
+            <Image
+              source={{ uri: selectedImage }}
+              style={{ width: "100%", height: "100%", borderRadius: 120 }}
+            />
+          )}
         </TouchableOpacity>
+        <TextInput
+          placeholder="Your name"
+          value={displayName}
+          onChangeText={setDisplayName}
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 2,
+            width: "100%",
+            marginTop: 40,
+          }}
+        />
       </View>
     </React.Fragment>
   );
