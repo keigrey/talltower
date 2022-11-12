9a2f06c96e86f36465c635196316a67ecb46b6e4
keigrey <keixgrey@gmail.com>
2022-11-12 21:36:05 +0900

    feat: Create uploadImage function to upload to firebase

--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
 import * as ImagePicker from "expo-image-picker";
+import { nanoid } from "nanoid";
+import "react-native-get-random-values";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "./firebase";
 
 const palette = {
   jet: "#0b0c0b", //background
@@ -28,11 +32,41 @@ export const theme = {
 };
 
 export async function pickImage() {
-  let result = ImagePicker.launchCameraAsync();
+  let result = await ImagePicker.launchCameraAsync();
   return result;
 }
 
 export async function askForPermission() {
   const { status } = await ImagePicker.requestCameraPermissionsAsync();
   return status;
+}
+
+export async function uploadImage(uri, path, fName) {
+  // Why are we using XMLHttpRequest? See:
+  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      console.log(e);
+      reject(new TypeError("Network request failed"));
+    };
+    xhr.responseType = "blob";
+    xhr.open("GET", uri, true);
+    xhr.send(null);
+  });
+
+  const fileName = fName || nanoid();
+  const imageRef = ref(storage, `${path}/${fileName}.jpeg`);
+
+  const snapshot = await uploadBytes(imageRef, blob, {
+    contentType: "image/jpeg",
+  });
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return { url, fileName };
 }
