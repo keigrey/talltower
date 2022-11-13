128f3549626fb02b270d387a15d5ccdaa029a2af
keigrey <keixgrey@gmail.com>
2022-11-13 21:45:19 +0900

    feat: Create Chats screen

---
+++ screens/Chats.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Chats() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "skyblue",
+      }}
+    >
+      <Text>Chats</Text>
+    </View>
+  );
+}
