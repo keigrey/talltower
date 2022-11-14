8e25498f9b89d2395fdf03f38c03536361fd0afe
keigrey <keixgrey@gmail.com>
2022-11-14 21:37:00 +0900

    feat: Query for users that have each others emails

--- screens/Chats.js
+++ screens/Chats.js
@@ -1,13 +1,42 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import GlobalContext from "../context/Context";
 
 export default function Chats() {
+  const { rooms, setRooms } = useContext(GlobalContext);
+  // TODO: rename to user for consistency
+  const { currentUser } = auth;
+
+  const chatsQuery = query(
+    collection(db, "rooms"),
+    where("participantsArray", "array-contains", currentUser.email)
+  );
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(chatsQuery, (querySnapshot) => {
+      const parsedChats = querySnapshot.docs
+        .filter((doc) => doc.data().lastMessage)
+        .map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+          userB: doc
+            .data()
+            .participants.find((p) => p.email !== currentUser.email),
+        }));
+      setRooms(parsedChats);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        padding: 5,
+        paddingRight: 10,
         backgroundColor: "skyblue",
       }}
     >
