c2d0f120b3c655963c0f1919ca7047e743dece41
keigrey <keixgrey@gmail.com>
2022-11-27 21:03:27 +0900

    style: Update Contacts style

--- screens/Contacts.js
+++ screens/Contacts.js
@@ -10,10 +10,8 @@ import { useRoute } from "@react-navigation/native";
 function ContactPreview({ contact, image }) {
   const { unfilteredRooms } = useContext(GlobalContext);
   const [user, setUser] = useState(contact);
-  // NEW VARIABLE
 
   useEffect(() => {
-    // TODO: instead of query can try to access the document directly
     const q = query(
       collection(db, "users"),
       where("email", "==", contact.email)
@@ -23,7 +21,6 @@ function ContactPreview({ contact, image }) {
       if (snapshot.docs.length) {
         const userDoc = snapshot.docs[0].data();
 
-        // TODO: change to (...user, userDoc)
         setUser((prevUser) => ({ ...prevUser, userDoc }));
       }
     });
@@ -45,13 +42,17 @@ function ContactPreview({ contact, image }) {
 }
 
 export default function Contacts() {
+  const {
+    theme: { colors },
+  } = useContext(GlobalContext);
+
   const contacts = useContacts();
   const route = useRoute();
   const image = route.params && route.params.image;
 
   return (
     <FlatList
-      style={{ flex: 1, padding: 10 }}
+      style={{ flex: 1, padding: 10, backgroundColor: colors.background }}
       data={contacts}
       keyExtractor={(_, i) => i}
       renderItem={({ item }) => <ContactPreview contact={item} image={image} />}
