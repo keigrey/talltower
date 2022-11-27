00ef23750f8ce19114d07e8b381e291041621fb4
keigrey <keixgrey@gmail.com>
2022-11-27 21:01:56 +0900

    style: Update Avatar style

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,20 +1,42 @@
-import { Image } from "react-native";
-import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import Context from "../context/Context";
 
 export default function Avatar({ size, user }) {
+  const {
+    theme: { colors },
+  } = useContext(Context);
+
+  const styles = StyleSheet.create({
+    imageOutline: {
+      width: size + 12,
+      height: size + 12,
+      padding: 15,
+      borderRadius: size + 12,
+      borderWidth: 2,
+      borderColor: colors.textGrey,
+      alignItems: "center",
+      justifyContent: "center",
+      margin: 5,
+    },
+    image: {
+      width: size,
+      height: size,
+      borderRadius: size,
+    },
+  });
+
   return (
-    <Image
-      source={
-        user.photoURL
-          ? { uri: user.photoURL }
-          : require("../assets/user-icon-square-dark.png")
-      }
-      resizeMode="cover"
-      style={{
-        width: size,
-        height: size,
-        borderRadius: size,
-      }}
-    />
+    <View style={styles.imageOutline}>
+      <Image
+        source={
+          user.photoURL
+            ? { uri: user.photoURL }
+            : require("../assets/user-icon-square-dark.png")
+        }
+        resizeMode="cover"
+        style={styles.image}
+      />
+    </View>
   );
 }
